627fd0d10450832d7d5df46757e8865a8c675529
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-04-07 00:11:21 -0300

    14.6. Usuários no Redux

--- src/core/hooks/useUsers.ts
+++ src/core/hooks/useUsers.ts
@@ -1,17 +1,32 @@
 import { User, UserService } from 'danielbonifacio-sdk';
 import { useCallback, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../store';
+// import { getAllUsers } from '../store/User.reducer';
+import * as UserActions from '../store/User.reducer';
 
 export default function useUsers() {
-  // trabalha com estado local
-  const [users, setUsers] = useState<User.Summary[]>([]);
+  // // trabalha com estado local
+  // const [users, setUsers] = useState<User.Summary[]>([]);
+  const dispatch = useDispatch();
+  const users = useSelector(
+    (state: RootState) => state.user.list
+  );
+  const fetching = useSelector(
+    (state: RootState) => state.user.fetching
+  );
 
   // Busca os dados
+  // const fetchUsers = useCallback(() => {
+  //   UserService.getAllUsers().then(setUsers);
+  // }, []);
   const fetchUsers = useCallback(() => {
-    UserService.getAllUsers().then(setUsers);
-  }, []);
+    dispatch(UserActions.getAllUsers());
+  }, [dispatch]);
 
   return {
     fetchUsers,
     users,
+    fetching,
   };
 }
