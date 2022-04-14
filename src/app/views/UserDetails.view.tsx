1f17d7a98c7612d8c5d7eb98c9742b81bf01b08f
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-04-14 19:30:12 -0300

    14.59. Descriptions com uma coluna

--- src/app/views/UserDetails.view.tsx
+++ src/app/views/UserDetails.view.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   Card,
   Col,
+  Descriptions,
   Progress,
   Row,
   Skeleton,
@@ -66,6 +67,22 @@ export default function UserDetailsView() {
               </div>
             ))}
           </Space>
+        </Col>
+        <Col xs={24} lg={12}>
+          <Descriptions column={1} bordered size={'small'}>
+            <Descriptions.Item label={'País'}>
+              {user.location.country}
+            </Descriptions.Item>
+            <Descriptions.Item label={'Estado'}>
+              {user.location.state}
+            </Descriptions.Item>
+            <Descriptions.Item label={'Cidade'}>
+              {user.location.city}
+            </Descriptions.Item>
+            <Descriptions.Item label={'Telefone'}>
+              {user.phone}
+            </Descriptions.Item>
+          </Descriptions>
         </Col>
       </Row>
     </>
