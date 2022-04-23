94e11fde3ddd4f39f9639232380be245090f58d8
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-04-23 08:59:27 -0300

    15.18. Desafio - Implementar busca com parâmetro no pagamento

--- src/app/views/PaymentDetails.view.tsx
+++ src/app/views/PaymentDetails.view.tsx
@@ -1,25 +1,47 @@
 import { Card, Divider } from 'antd';
+import { ResourceNotFoundError } from 'danielbonifacio-sdk/dist/errors';
 import moment from 'moment';
 import { useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import usePayment from '../../core/hooks/usePayment';
+import NotFoundError from '../components/NotFoundError';
 import PaymentBonuses from '../features/PaymentBonuses';
 import PaymentHeader from '../features/PaymentHeader';
 import PaymentPosts from '../features/PaymentPosts';
 
 export default function PaymentDetailsView() {
+  const params = useParams<{ id: string }>();
+  const history = useHistory();
+
   const {
     fetchPayment,
     fetchPosts,
     fetchingPayment,
     fetchingPosts,
+    postsNotFound,
+    paymentNotFound,
     payment,
     posts,
   } = usePayment();
 
   useEffect(() => {
-    fetchPosts(2);
-    fetchPayment(2);
-  }, [fetchPosts, fetchPayment]);
+    const paymentId = Number(params.id);
+    if (isNaN(paymentId)) {
+      return history.push('/pagamentos');
+    } else {
+      fetchPosts(paymentId);
+      fetchPayment(paymentId);
+    }
+  }, [fetchPosts, fetchPayment, params.id, history]);
+
+  if (paymentNotFound)
+    return (
+      <NotFoundError
+        title='Pagamento não encontrado'
+        actionDestination='/pagamentos'
+        actionTitle={'Ir para a lista de pagamentos'}
+      />
+    );
 
   return (
     <>
--- src/core/hooks/usePayment.ts
+++ src/core/hooks/usePayment.ts
@@ -1,6 +1,6 @@
 import { Payment, PaymentService, Post } from 'danielbonifacio-sdk';
-import { useCallback } from 'react';
-import { useState } from 'react';
+import { ResourceNotFoundError } from 'danielbonifacio-sdk/dist/errors';
+import { useCallback, useState } from 'react';
 
 export default function usePayment() {
   const [posts, setPosts] = useState<Post.WithEarnings[]>([]);
@@ -9,11 +9,19 @@ export default function usePayment() {
   const [fetchingPosts, setFetchingPosts] = useState(false);
   const [fetchingPayment, setFetchingPayment] = useState(false);
 
+  const [paymentNotFound, setPaymentNotFound] = useState(false);
+  const [postsNotFound, setPostsNotFound] = useState(false);
+
   const fetchPayment = useCallback(async (paymentId: number) => {
     try {
       setFetchingPayment(true);
       const payment = await PaymentService.getExistingPayment(paymentId);
       setPayment(payment);
+    } catch (error) {
+      if (error instanceof ResourceNotFoundError) {
+        setPaymentNotFound(true);
+      }
+      throw error;
     } finally {
       setFetchingPayment(false);
     }
@@ -24,6 +32,11 @@ export default function usePayment() {
       setFetchingPosts(true);
       const posts = await PaymentService.getExistingPaymentPosts(paymentId);
       setPosts(posts);
+    } catch (error) {
+      if (error instanceof ResourceNotFoundError) {
+        setPostsNotFound(true);
+      }
+      throw error;
     } finally {
       setFetchingPosts(false);
     }
@@ -34,6 +47,8 @@ export default function usePayment() {
     fetchPosts,
     fetchingPayment,
     fetchingPosts,
+    paymentNotFound,
+    postsNotFound,
     posts,
     payment,
   };
