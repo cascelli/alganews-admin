a186b50164d29232556406ba5c29fc12fe5ea30b
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-04-25 15:33:19 -0300

    16.7. Base do gerenciamento de categorias

--- src/app/views/CashFlowExpenses.view.tsx
+++ src/app/views/CashFlowExpenses.view.tsx
@@ -51,18 +51,34 @@ export default function CashFlowExpensesView() {
 */
 
 /* === Impelmentação com Flux / Redux === */
-import { Button, Divider, Row, Space, Tooltip, Typography } from 'antd';
-import { InfoCircleFilled } from '@ant-design/icons';
+import { Button, Divider, Modal, Row, Space, Tooltip, Typography } from 'antd';
+import { InfoCircleFilled, TagOutlined } from '@ant-design/icons';
 import EntriesList from '../features/EntriesList';
 import useCashFlow from '../../core/hooks/useCashFlow';
 import DoubleConfirm from '../components/DoubleConfirm';
+import { useCallback, useState } from 'react';
+import EntryCategoryManager from '../features/EntryCategoryManager';
+
 const { Title, Text } = Typography;
 
 export default function CashFlowExpensesView() {
   const { selected, removeEntries } = useCashFlow('EXPENSE');
+
+  const [showCategoryModal, setShowCategoryModal] = useState(false);
+
+  const openCategoryModal = useCallback(() => setShowCategoryModal(true), []);
+  const closeCategoryModal = useCallback(() => setShowCategoryModal(false), []);
+
   return (
     <>
-      <Row>
+      <Modal
+        visible={showCategoryModal}
+        onCancel={closeCategoryModal}
+        closeIcon={<></>}
+      >
+        <EntryCategoryManager />
+      </Modal>
+      <Row justify={'space-between'} style={{ marginBottom: 16 }}>
         <DoubleConfirm
           popConfirmTitle={`Remover ${
             selected.length > 1
@@ -81,6 +97,13 @@ export default function CashFlowExpensesView() {
             Remover
           </Button>
         </DoubleConfirm>
+        <Button
+          type={'primary'}
+          icon={<TagOutlined />}
+          onClick={openCategoryModal}
+        >
+          Categorias
+        </Button>
       </Row>
       <Space direction={'vertical'}>
         <Title level={3}>Recuperando entradas do mês de agosto</Title>
