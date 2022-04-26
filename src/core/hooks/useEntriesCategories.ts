bfeb9e1215c8e05c7b8276509befe22da051f6fd
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-04-26 09:37:03 -0300

    16.12. dispatch().unwrap()

--- src/core/hooks/useEntriesCategories.ts
+++ src/core/hooks/useEntriesCategories.ts
@@ -1,11 +1,12 @@
 import { CashFlow } from 'danielbonifacio-sdk';
 import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../store';
+import { AppDispatch, RootState } from '../store';
 import * as CategoryActions from '../store/EntriesCategory.slice';
 
 export default function useEntriesCategories() {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
   const expenses = useSelector((s: RootState) => s.cashFlow.category.expenses);
   const revenues = useSelector((s: RootState) => s.cashFlow.category.revenues);
 
@@ -16,7 +17,8 @@ export default function useEntriesCategories() {
 
   const createCategory = useCallback(
     (category: CashFlow.CategoryInput) =>
-      dispatch(CategoryActions.createCategory(category)),
+      // dispatch(CategoryActions.createCategory(category)),
+      dispatch(CategoryActions.createCategory(category)).unwrap(),
     [dispatch]
   );
 
